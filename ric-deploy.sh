f557563df710e462789740d17b9563b99bc0b387
Chubru <sashach02@yandex.ru>
2021-10-10 02:39:46 +0300

    .

--- ric-deploy.sh
+++ ric-deploy.sh
@@ -7,17 +7,25 @@ if [ "$1" == "install" ]
     git clone http://gerrit.o-ran-sc.org/r/it/dep
     cd dep
     git submodule update --init --recursive --remote
-    
-    sed -i 's/"repository: kong-docker-kubernetes-ingress-controller.bintray.io/kong-ingress-controller"/""image: kong/kubernetes-ingress-controller:0.7.0""/g' /ric-dep/helm/infrastructure/subcharts/kong/values.yaml
 
 
-    ric-dep/helm/infrastructure/subcharts/kong/values.yaml
     cd tools/k8s/bin
     ./gen-cloud-init.sh
 
     curl -L https://git.io/get_helm.sh | bash
 
-    helm init --client-only --skip-re
+    helm init --client-only
     helm repo rm stable
     helm repo add stable https://charts.helm.sh/stable
+
+
+
+    tmp1="repository: kong-docker-kubernetes-ingress-controller.bintray.io/kong-ingress-controller"
+    tmp2="repository: kong/kubernetes-ingress-controller"
+    
+    sed -i "s#${tmp1}#${tmp2}#g" ../../../ric-dep/helm/infrastructure/subcharts/kong/values.yaml
 fi
+
+
+
+grep -Rw . -e 'repository: kong-docker-kubernetes-ingress-controller.bintray.io/kong-ingress-controller'
