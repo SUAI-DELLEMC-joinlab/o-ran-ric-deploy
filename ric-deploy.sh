6191494a1b00ee9429f1d5eab4ddf7bb0a01ffbd
Chubru <sashach02@yandex.ru>
2021-10-10 03:09:21 +0300

    .

--- ric-deploy.sh
+++ ric-deploy.sh
@@ -14,18 +14,31 @@ if [ "$1" == "install" ]
 
     curl -L https://git.io/get_helm.sh | bash
 
-    helm init --client-only
-    helm repo rm stable
-    helm repo add stable https://charts.helm.sh/stable
-
-
 
     tmp1="repository: kong-docker-kubernetes-ingress-controller.bintray.io/kong-ingress-controller"
     tmp2="repository: kong/kubernetes-ingress-controller"
     
     sed -i "s#${tmp1}#${tmp2}#g" ../../../ric-dep/helm/infrastructure/subcharts/kong/values.yaml
+
+    ./k8s-1node-cloud-init-k_1_16-h_2_17-d_cur.sh 
+
+fi
+
+if [ "$1" == "deploy" ]
+    then
+
+    helm init --client-only
+    helm repo rm stable
+    helm repo add stable https://charts.helm.sh/stable
+
+
+    cd dep/bin
+
+    cd dep/bin
+    ./deploy-ric-platform -f ../RECIPE_EXAMPLE/PLATFORM/example_recipe.yaml
+    kubectl get pods -n ricplt
+
+
 fi
 
 
-
-grep -Rw . -e 'repository: kong-docker-kubernetes-ingress-controller.bintray.io/kong-ingress-controller'
